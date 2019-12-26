6f833cdf5371dd2c1d397d0bb8cda309217673c9
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-12-26 12:11:25 +0100

    [scene_detection_action/ObjectDetection] Fixed object header

--- ros/src/mas_perception_libs/scene_detection_action.py
+++ ros/src/mas_perception_libs/scene_detection_action.py
@@ -186,7 +186,8 @@ class ObjectDetectionActionServer(object):
             detected_obj.probability = confidences[index]
             detected_obj.pointcloud = cropped_cloud
             detected_obj.rgb_image = cloud_msg_to_image_msg(cropped_cloud)
-            detected_obj.pose.header = plane.header
+            detected_obj.pose.header.frame_id = self._target_frame
+            detected_obj.pose.header.stamp = rospy.Time.now()
             detected_obj.pose.pose.position = box_msg.center
             detected_obj.pose.pose.position.x = mean_coord[0]
             detected_obj.pose.pose.orientation.w = 1.0
